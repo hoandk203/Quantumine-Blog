55036d42f20b21fb223dbeb2ef2c5b70455e5a4e
hoandk203 <hoanyttv@gmail.com>
2025-11-17 18:30:24 +0700

    prod docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone', // Enable standalone output for Docker
   images: {
     domains: ['localhost', 'example.com'],
     remotePatterns: [
